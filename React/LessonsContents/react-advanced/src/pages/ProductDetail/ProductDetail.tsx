d219e17cc138437d87827e8001b035ef2b512fa6
Bedirhan Balcı <bedirhanbalci@outlook.com>
2023-12-29 01:37:54 +0300

    React Advanced with TypeScript

---
+++ React/LessonsContents/react-advanced/src/pages/ProductDetail/ProductDetail.tsx
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from "react";
+import { useLocation, useParams } from "react-router-dom";
+import { ProductModel } from "../../models/responses/ProductModel";
+import ProductService from "../../services/ProductService";
+
+type Props = {};
+
+const ProductDetail = (props: Props) => {
+  const params = useParams<{ id: string }>(); // location
+  // const location = useLocation();    query string
+  const [product, setProduct] = useState<ProductModel>();
+
+  useEffect(() => {
+    // let query = new URLSearchParams(location.search);
+    if (params.id) {
+      ProductService.getById(parseInt(params.id)).then(response => {
+        setProduct(response.data);
+      });
+    }
+    // console.log(query.get("abc"));
+    // console.log(query.get("id"));
+  }, []);
+
+  return (
+    <div>
+      <p>{product?.title}</p>
+    </div>
+  );
+};
+export default ProductDetail;
